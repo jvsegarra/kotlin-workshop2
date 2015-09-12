0da3f2bd623ccd4e77fc6cb7e8d1c539ff7f2efa
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-12 20:41:53 +0200

    gettingStarted/exercise_d

--- src/gettingStarted/exercise_d.kt
+++ src/gettingStarted/exercise_d.kt
@@ -2,3 +2,6 @@ package gettingStarted
 
 
 // TODO: [gettingStarted] Create a function with a nullable type as parameter and force usage of it even if not null
+fun withNullableType(param: String?) {
+   param!!.capitalize()
+}
