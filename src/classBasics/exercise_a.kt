7ba9c1bb1fdbef2d5e380baa38f7630524f57d79
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-12 23:27:50 +0200

    classBasics/exercise_a

--- src/classBasics/exercise_a.kt
+++ src/classBasics/exercise_a.kt
@@ -1,5 +1,7 @@
 package classBasics
 
 // TODO: [classBasics] Create a class with a property of type string readable/writable
-
+class classWithPropertyReadableWritable(var readableWritable: String) {
+    
+}
 
