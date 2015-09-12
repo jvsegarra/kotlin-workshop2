d22e4503a0cf43b90b5689f2edf33035b5fe75ff
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-12 02:33:59 +0200

    gettingStarted/exercise_a

--- src/gettingStarted/exercise_a.kt
+++ src/gettingStarted/exercise_a.kt
@@ -1,4 +1,14 @@
 package gettingStarted
 
+import kotlin.platform.platformStatic
+
 
 // TODO: [gettingStarted] Write a main function that prints println in Kotlin and run it
+// You can have multiple main functions in your project but only one main function per package
+// We already have a main function in exercise B, so this is a workaround
+object Main {
+    @platformStatic
+    fun main(args: Array<String>) {
+        println("println")
+    }
+}
