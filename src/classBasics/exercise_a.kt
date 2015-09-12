a27dbd9822043559f77230c0e1b196ac7207f9e3
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-12 23:45:08 +0200

    classBasics/exercise_c

--- src/classBasics/exercise_a.kt
+++ src/classBasics/exercise_a.kt
@@ -2,6 +2,6 @@ package classBasics
 
 // TODO: [classBasics] Create a class with a property of type string readable/writable
 class classWithPropertyReadableWritable(var readableWritable: String) {
-    
+
 }
 
--- src/classBasics/exercise_c.kt
+++ src/classBasics/exercise_c.kt
@@ -1,4 +1,7 @@
 package classBasics
 
 // TODO: [classBasics] Create a class with immutable property name
+class classWithImmutableProperty(val name: String) {
+
+}
 
