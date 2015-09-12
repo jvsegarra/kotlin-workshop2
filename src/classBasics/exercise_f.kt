3cec475eba661c35993a0c15299a451d56b5f3ed
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-13 00:52:27 +0200

    classBasics/exercise_f

--- src/classBasics/exercise_f.kt
+++ src/classBasics/exercise_f.kt
@@ -1,3 +1,11 @@
 package classBasics
 
 // TODO: [classBasics] Create a class with an inner class and see how you can reference a property of the outer class
+class Outer {
+    private val outerProperty: Int = 1
+    inner class Nested {
+        fun foo() = outerProperty
+    }
+}
+
+val demo = Outer().Nested().foo() // == 1
