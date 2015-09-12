9ae62b26970a1562430eb7386bfa86d5dc162118
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-13 00:21:24 +0200

    classBasics/exercise_e

--- src/classBasics/exercise_e.kt
+++ src/classBasics/exercise_e.kt
@@ -1,6 +1,20 @@
 package classBasics
 
 // TODO: [classBasics] Create an instance of previous class and call toString(). What happens?
+class createUserInstances() {
+    fun createInstances() {
+        val userKotlin = UserKotlin("jv", "segarra", 35)
+        println(userKotlin.toString())
+    }
+}
+
 
 // TODO: [classBasics] What happens when you compare two instances?
+class compareUserInstances() {
+    fun compareInstances() {
+        val userKotlin1 = UserKotlin("jv", "segarra", 35)
+        val userKotlin2 = UserKotlin("jv", "segarra", 35)
+        println(userKotlin1.equals(userKotlin2) )
+    }
+}
 
