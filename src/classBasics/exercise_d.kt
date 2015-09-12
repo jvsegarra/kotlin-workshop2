f2eb5283eeb756f2e319dc46223c8b998657fb89
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-13 01:28:55 +0200

    objects/exercise_c

--- src/classBasics/exercise_d.kt
+++ src/classBasics/exercise_d.kt
@@ -2,4 +2,4 @@ package classBasics
 
 
 // TODO: [classBasics] Convert the User Java Class to Kotlin.
-class UserKotlin(val firstName: String, val lastName: String, val age: Int)
+open class UserKotlin(val firstName: String, val lastName: String, val age: Int)
--- src/objects/exercise_c.kt
+++ src/objects/exercise_c.kt
@@ -1,5 +1,10 @@
 package objects
 
+import classBasics.UserKotlin
 
 
 // TODO: [objects] Create an anonymous object that inherits from the user class created in classBasics
+val anonymousObjectInherited = object : UserKotlin("jv2", "segarra2", 35) {
+    val property1: Int = 0
+    val property2: String = "example"
+}
