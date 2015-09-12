8598b4288c43f1257a122961113e9b34491cde51
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-12 20:37:53 +0200

    gettingStarted/exercise_c

--- src/gettingStarted/exercise_c.kt
+++ src/gettingStarted/exercise_c.kt
@@ -1,4 +1,16 @@
 package gettingStarted
 
+import java.io.File
+
 
 // TODO: [gettingStarted] Convert the code in FileCoder to Kotlin
+class Filecoder {
+    fun printDirectory() {
+        val files = File("Something")
+        val fileList = files.listFiles()
+
+        if (fileList != null) {
+            println(fileList.size())
+        }
+    }
+}
