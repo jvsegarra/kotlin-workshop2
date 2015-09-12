1df88c67927a9b9d12b8002a8b2f98d3b455f0f4
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-13 01:24:34 +0200

    objects/exercise_b

--- src/objects/exercise_b.kt
+++ src/objects/exercise_b.kt
@@ -2,3 +2,7 @@ package objects
 
 
 // TODO: [objects] Create an anonymous object with two properties
+val anonymousObject = object {
+    val property1: Int = 0
+    val property2: String = "example"
+}
