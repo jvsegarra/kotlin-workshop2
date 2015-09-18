70d2fb9f60d3b0e4704c0b00b0e555be0531fdcc
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-18 04:07:49 +0200

    traits/exercise_a

--- src/traits/exercise_a.kt
+++ src/traits/exercise_a.kt
@@ -2,3 +2,7 @@ package traits
 
 
 // TODO: [traits] Declare a Customer Repository as a trait with methods getAll and findByName
+public interface customerRepository {
+    fun getAll() = println("getAll")
+    fun findByName() = println("findByName")
+}
