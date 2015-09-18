f86cc46327e01db8dc8383967eb8c3e40e0dd2a5
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-18 17:30:22 +0200

    interop/exercise_a

--- src/interop/exercise_a.kt
+++ src/interop/exercise_a.kt
@@ -1,3 +1,11 @@
 package interop
 
+import helpers.customerList
+import junit.framework.Assert
+import org.junit.Test
+
 // TODO: [interop] Write a unit test using JUnit and Kotlin assert to test customerList returns non empty list
+
+class Tester {
+    Test fun testCustomerListIsNotEmpty() = Assert.assertNotNull(customerList())
+}
