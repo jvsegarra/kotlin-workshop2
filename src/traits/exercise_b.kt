68c9df26620b597504fc4c98012f6ddf54ae3dbb
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-18 04:08:01 +0200

    traits/exercise_b

--- src/traits/exercise_b.kt
+++ src/traits/exercise_b.kt
@@ -1,3 +1,15 @@
 package traits
 
+import kotlin.platform.platformStatic
+
 // TODO: [traits] Create a Customer Controller class that delegates the customer repository functionality to the trait created in exercise_a
+class CustomerController : customerRepository {}
+
+object Main {
+    @platformStatic
+    fun main(args: Array<String>) {
+        val c = CustomerController()
+        c.getAll()
+        c.findByName()
+    }
+}
