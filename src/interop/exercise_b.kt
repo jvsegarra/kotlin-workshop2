03d0f360aec8b38c5b5f2acfb952230f05794bae
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-18 22:06:11 +0200

    interop/exercise_b

--- src/interop/exercise_b.kt
+++ src/interop/exercise_b.kt
@@ -1,3 +1,34 @@
 package interop
 
+import org.apache.http.HttpResponse
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClientBuilder
+import traits.CustomerController
+import kotlin.platform.platformStatic
+
 // TODO: [interop] Create a smaller wrapper around Apache HttpClient to perform a GET and return a string as result
+class SmallWrapper() {
+    fun get(): String {
+        val url: String = "http://www.google.com/search?q=httpClient"
+
+        val client: HttpClient = HttpClientBuilder.create().build();
+        val request: HttpGet = HttpGet(url);
+
+        // add request header
+        //request.addHeader("User-Agent", USER_AGENT);
+        val response: HttpResponse = client.execute(request);
+
+        var result: String = "Response Code : ${response.getStatusLine().getStatusCode()}";
+
+        return result
+    }
+}
+
+object Main {
+    @platformStatic
+    fun main(args: Array<String>) {
+        val c = SmallWrapper()
+        println(c.get())
+    }
+}
