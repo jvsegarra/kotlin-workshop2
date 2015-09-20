dc9a7719eb716e94b582e9fef0abed7ad4d44aaf
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-20 03:16:46 +0200

    functions/exercise_c

--- src/functions/exercise_c.kt
+++ src/functions/exercise_c.kt
@@ -1,5 +1,20 @@
 package functions
 
+import kotlin.platform.platformStatic
+
 
 // TODO: [functions] Extend the StringBuilder class and override the behavior of the += operator to append a string
+fun StringBuilder.plusAssign(string: String) {
+    this.append(string)
+}
 
+object MainC {
+    @platformStatic
+    fun main(args: Array<String>) {
+        var s: StringBuilder = StringBuilder("hola")
+        println(s)
+
+        s += " y adios"
+        println(s)
+    }
+}
