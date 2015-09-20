6a2c74750a45014f6bc1a051fc920e579b4d7b84
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-20 14:16:11 +0200

    functions/exercise_h

--- src/functions/exercise_h.kt
+++ src/functions/exercise_h.kt
@@ -3,3 +3,6 @@ package functions
 
 // TODO: [functions] Create a function that takes a string as first parameter and a function that takes no parameters and
 //       returns nothing as second parameter. Call it using convention of omitting () on second parameter
+fun lambdaParams(firstParam: String, lambdaParam: () -> Unit ) {}
+
+val f = lambdaParams("string", {})
