f75544d01f72e50854d2cc762482489ee9ff93a2
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-20 03:50:48 +0200

    functions/exercise_f

--- src/functions/exercise_f.kt
+++ src/functions/exercise_f.kt
@@ -1,3 +1,4 @@
 package functions
 
 // TODO: [functions] Create a function that takes an infinite number of functions as a parameter that takes no parameters and returns nothing
+fun lambdaWithInfiniteParams(vararg lambdaParam: () -> Unit ) {}
