2ffaaa6cddcf8a21cb9cc42943a79d3b6c3845ac
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-20 13:57:13 +0200

    functions/exercise_g

--- src/functions/exercise_g.kt
+++ src/functions/exercise_g.kt
@@ -1,3 +1,6 @@
 package functions
 
 // TODO: [functions] Create an extension function literal to the string class that replaces white spaces with a string
+var spaceReplaced = fun String.(char: Char): String = this.replace(' ', char)
+
+val output = "This is an example".spaceReplaced('_')
