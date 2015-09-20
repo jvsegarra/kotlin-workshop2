c5a8ece22182fd5ccb3a2ae3d6e0924349cdf4da
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-20 14:27:01 +0200

    functional/exercise_a

--- src/functional/exercise_a.kt
+++ src/functional/exercise_a.kt
@@ -1,4 +1,9 @@
 package functional
 
+import helpers.Customer
+
 
 // TODO: [functional] Create a function that given a list of Customers returns all customers matching a specific name
+fun filterCustomers(listOfCustomers: List<Customer>, name: String) : List<Customer> {
+    return listOfCustomers.filter { it -> it.name == name }
+}
