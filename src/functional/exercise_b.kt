346fa40f9226a923d1919f85231b115ad92eed1b
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-21 00:46:37 +0200

    functional/exercise_b

--- src/functional/exercise_b.kt
+++ src/functional/exercise_b.kt
@@ -1,4 +1,9 @@
 package functional
 
+import helpers.Customer
+
 
 // TODO: [functional] Create a function that given a list of Customers, it converts to uppercase all those that live in a specific country defined also as parameter
+fun filterAndConvertCustomers(listOfCustomers: List<Customer>, country: String): List<String> {
+    return listOfCustomers.filter { it -> it.country == country }.map { it.country.toUpperCase() }
+}
