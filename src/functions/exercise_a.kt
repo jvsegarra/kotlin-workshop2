caa8a364b3bddf7d30a64287c0b8a8be95bf632d
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-20 02:30:37 +0200

    functions/exercise_a

--- src/functions/exercise_a.kt
+++ src/functions/exercise_a.kt
@@ -1,5 +1,16 @@
 package functions
 
+import kotlin.platform.platformStatic
+
 // TODO: [functions] Create a function called sum that takes two parameters and adds them. Call it in infix notation
+fun Int.sum(param: Int): Int {
+    return this + param
+}
 
-
+object Main {
+    @platformStatic
+    fun main(args: Array<String>) {
+        var x = 1
+        println(x sum 2)
+    }
+}
