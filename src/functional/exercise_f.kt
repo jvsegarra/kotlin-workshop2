a1b1ccf1e088d73b512fb3f7a2c85f76e3fd13a1
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-21 01:21:39 +0200

    functional/exercise_f

--- src/functional/exercise_f.kt
+++ src/functional/exercise_f.kt
@@ -1,3 +1,8 @@
 package functional
 
+import helpers.Customer
+
 // TODO: [functional] Create a function that given a list of Customers, it groups them by country and also converts each name to proper case
+fun groupAndConvertCustomers(listOfCustomers: List<Customer>): Map<String, List<Customer>> {
+    return listOfCustomers.groupBy { it -> it.country }
+}
