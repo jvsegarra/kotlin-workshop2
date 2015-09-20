9ea8b381a5976ec5e303efddce850cc27dc5e9ec
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-21 00:54:35 +0200

    functional/exercise_c

--- src/functional/exercise_c.kt
+++ src/functional/exercise_c.kt
@@ -1,4 +1,11 @@
 package functional
 
+import java.util.*
+
 
 // TODO: [functional] Create a function that prints "[key is value]" of hashmap entries to the console. Use multideclarations
+fun prints(map: HashMap<Int, String>) {
+    for ((key, value) in map) {
+        println("$key is $value")
+    }
+}
