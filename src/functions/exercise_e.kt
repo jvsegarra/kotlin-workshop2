0fea44295502603c0a3fb0ac5d74352d09fe2df3
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-20 03:47:33 +0200

    functions/exercise_e

--- src/functions/exercise_e.kt
+++ src/functions/exercise_e.kt
@@ -2,3 +2,4 @@ package functions
 
 
 // TODO: [functions] Create a function that takes another function as parameter that takes an int, a string and returns a string
+fun lambdaWithDifferentTypes(lambdaParam: (Int, String) -> String ) {}
