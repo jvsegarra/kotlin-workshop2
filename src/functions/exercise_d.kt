cb35a3fb5462bdfa809f4c38fa1c6f6e88ad4f12
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-20 03:45:46 +0200

    functions/exercise_d

--- src/functions/exercise_d.kt
+++ src/functions/exercise_d.kt
@@ -1,3 +1,4 @@
 package functions
 
 // TODO: [functions] Create a function that takes another function as parameter that takes no parameters and returns nothing
+fun lambda(lambdaParam: () -> Unit ) {}
