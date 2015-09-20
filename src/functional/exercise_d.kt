5b61c476c814e57d54d2b0c14c082b83f20c3cf0
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-21 00:58:56 +0200

    functional/exercise_d

--- src/functional/exercise_d.kt
+++ src/functional/exercise_d.kt
@@ -1,4 +1,15 @@
 package functional
 
+import java.util.concurrent.locks.Lock
+
 
 // TODO: [functional] Create a function that allows some code to be executed protected with a lock
+fun lock<T>(lock: Lock, body: () -> T): T {
+    lock.lock()
+    try {
+        return body()
+    }
+    finally {
+        lock.unlock()
+    }
+}
