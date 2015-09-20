6f8a936c264a86879ef8e590dd2eb41ef373b2de
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-21 01:10:58 +0200

    functional/exercise_e

--- src/functional/exercise_e.kt
+++ src/functional/exercise_e.kt
@@ -1,3 +1,17 @@
-package functions
+package functional
 
 // TODO: [functional] Create FizzBuzz
+fun Int.divides(d: Int): Boolean = this % d == 0
+
+fun main(args: Array<String>): Unit {
+    for (i in 0..100) {
+        println(
+                when {
+                    i divides 15 -> "fizzbuzz"
+                    i divides 3 -> "fizz"
+                    i divides 5 -> "buzz"
+                    else -> "$i"
+                }
+        )
+    }
+}
