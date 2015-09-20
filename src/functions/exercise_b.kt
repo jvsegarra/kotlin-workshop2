9ee1f6582481aea58048e40edc9acbaa9183e407
Josep Vicent Segarra Sola <josevi@MacBook-Pro-de-Josep.local>
2015-09-20 02:37:46 +0200

    functions/exercise_b

--- src/functions/exercise_b.kt
+++ src/functions/exercise_b.kt
@@ -1,3 +1,19 @@
 package functions
 
+import kotlin.platform.platformStatic
+
 // TODO: [functions] Extend the StringBuilder class by adding an emtpy() function that clears all entries
+fun StringBuilder.empty() {
+    this.setLength(0)
+}
+
+object MainB {
+    @platformStatic
+    fun main(args: Array<String>) {
+        var s: StringBuilder = StringBuilder("hola")
+        println(s)
+
+        s.empty()
+        println(s)
+    }
+}
